45a8dc03592dd2d3adffd7206b57f92e29584cb4
David Denton <denton.david@gmail.com>
2016-03-07 14:17:40 +0000

    daveD - added delete by id

--- src/main/scala/io/fintrospect/todo/TodoModule.scala
+++ src/main/scala/io/fintrospect/todo/TodoModule.scala
@@ -28,6 +28,17 @@ class TodoModule(todoDb: TodoDb) extends ServerRoutes[Response] {
     }
   }
 
+  private def delete(id: String) = Service.mk {
+    rq: Request => {
+      todoDb.get(id) match {
+        case Some(t) =>
+          todoDb.delete(t.id)
+          Ok(encode(t))
+        case None => NotFound(encode(TodoNotFound(id)))
+      }
+    }
+  }
+
   private def deleteAll = Service.mk {
     rq: Request => {
       todoDb.list().foreach(todo => todoDb.delete(todo.id))
@@ -46,22 +57,22 @@ class TodoModule(todoDb: TodoDb) extends ServerRoutes[Response] {
   private def update(id: String) = Service.mk {
     rq: Request =>
       todoDb.get(id) match {
-        case Some(currentTodo) => {
+        case Some(currentTodo) =>
           val patch = todoSpec <-- rq
           todoDb.delete(id)
           val updated = patch.modify(currentTodo)
           todoDb.save(updated)
           Ok(encode(updated))
-        }
         case None => NotFound(encode(TodoNotFound(id)))
       }
   }
 
-  val module = ModuleSpec(Root / "todos", Swagger2dot0Json(ApiInfo("Todo backend API", "1.0")))
+  val module = ModuleSpec(Root / "todos", Swagger2dot0Json(ApiInfo("todo backend API", "1.0")))
     .withDescriptionPath(_ / "api")
-    .withRoute(RouteSpec().at(Get) bindTo listAll)
-    .withRoute(RouteSpec().at(Get) / id bindTo lookup)
-    .withRoute(RouteSpec().body(todoSpec).at(Post) bindTo add)
-    .withRoute(RouteSpec().body(todoSpec).at(Patch) / id bindTo update)
-    .withRoute(RouteSpec().at(Delete) bindTo deleteAll)
+    .withRoute(RouteSpec("lists all todos in the system").at(Get) bindTo listAll)
+    .withRoute(RouteSpec("get a todo by id").at(Get) / id bindTo lookup)
+    .withRoute(RouteSpec("add a new todo by supplying fields").body(todoSpec).at(Post) bindTo add)
+    .withRoute(RouteSpec("patch an existing todo").body(todoSpec).at(Patch) / id bindTo update)
+    .withRoute(RouteSpec("delete an existing todo").at(Delete) / id bindTo delete)
+    .withRoute(RouteSpec("delete all todos in the system").at(Delete) bindTo deleteAll)
 }
