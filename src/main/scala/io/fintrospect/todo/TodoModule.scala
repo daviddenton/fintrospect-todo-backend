73ded3a4b584fba8105ed338dcec9aae9217c0e4
David Denton <denton.david@gmail.com>
2016-03-07 11:55:49 +0000

    daveD - delete endpoint now deletes all

--- src/main/scala/io/fintrospect/todo/TodoModule.scala
+++ src/main/scala/io/fintrospect/todo/TodoModule.scala
@@ -1,7 +1,5 @@
 package io.fintrospect.todo
 
-import java.util.UUID
-
 import com.twitter.finagle.Service
 import com.twitter.finagle.http.Method.{Delete, Get, Patch, Post}
 import com.twitter.finagle.http.Status.{Conflict, NotFound, Ok}
@@ -21,7 +19,7 @@ class TodoModule(todoDb: TodoDb) extends ServerRoutes[Response] {
 
   private val todoSpec = Body(BodySpec[Todo](Option("A todo entity"), APPLICATION_JSON, s => decode[Todo](parse(s)), todo => compact(encode(todo))))
 
-  private def list = Service.mk { r: Request => Ok(encode(todoDb.list())) }
+  private def listAll = Service.mk { r: Request => Ok(encode(todoDb.list())) }
 
   private def lookup(id: String) = Service.mk { r: Request =>
     todoDb.get(id) match {
@@ -30,41 +28,43 @@ class TodoModule(todoDb: TodoDb) extends ServerRoutes[Response] {
     }
   }
 
-  private def delete(id: String) = Service.mk { r: Request =>
-    todoDb.get(id) match {
-      case Some(t) => todoDb.delete(id); Ok(encode(t))
-      case None => NotFound(encode(TodoNotFound(id)))
+  private def deleteAll = Service.mk {
+    r: Request => {
+      todoDb.list().foreach(todo => todoDb.delete(todo.id));
+      Ok()
     }
   }
 
-  private def add = Service.mk { r: Request =>
-    val newTodo = todoSpec <-- r
-    todoDb.get(newTodo.id) match {
-      case None => {
-        todoDb.save(newTodo)
-        Ok(encode(newTodo))
+  private def add = Service.mk {
+    r: Request =>
+      val newTodo = todoSpec <-- r
+      todoDb.get(newTodo.id) match {
+        case None => {
+          todoDb.save(newTodo)
+          Ok(encode(newTodo))
+        }
+        case Some(todo) => Conflict(encode(todo))
       }
-      case Some(todo) => Conflict(encode(todo))
-    }
   }
 
-  private def update(id: String) = Service.mk { r: Request =>
-    todoDb.get(id) match {
-      case Some(currentTodo) => {
-        val updated = todoSpec <-- r
-        todoDb.delete(id)
-        todoDb.save(updated)
-        Ok(encode(updated))
+  private def update(id: String) = Service.mk {
+    r: Request =>
+      todoDb.get(id) match {
+        case Some(currentTodo) => {
+          val updated = todoSpec <-- r
+          todoDb.delete(id)
+          todoDb.save(updated)
+          Ok(encode(updated))
+        }
+        case None => NotFound(encode(TodoNotFound(id)))
       }
-      case None => NotFound(encode(TodoNotFound(id)))
-    }
   }
 
   val module = ModuleSpec(Root / "todos", Swagger2dot0Json(ApiInfo("Todo backend API", "1.0")))
     .withDescriptionPath(_ / "api")
-    .withRoute(RouteSpec().at(Get) bindTo list)
+    .withRoute(RouteSpec().at(Get) bindTo listAll)
     .withRoute(RouteSpec().at(Get) / id bindTo lookup)
     .withRoute(RouteSpec().body(todoSpec).at(Post) bindTo add)
     .withRoute(RouteSpec().body(todoSpec).at(Patch) / id bindTo update)
-    .withRoute(RouteSpec().at(Delete) / id bindTo delete)
+    .withRoute(RouteSpec().at(Delete) bindTo deleteAll)
 }
