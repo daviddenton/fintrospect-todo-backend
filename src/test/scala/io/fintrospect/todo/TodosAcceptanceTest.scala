5adbbfa5688a74662235d52905c8c7d0ab800668
David Denton <denton.david@gmail.com>
2016-03-08 13:42:07 +0000

    daveD - added coverage for delete all and not founds

--- src/test/scala/io/fintrospect/todo/TodosAcceptanceTest.scala
+++ src/test/scala/io/fintrospect/todo/TodosAcceptanceTest.scala
@@ -1,6 +1,6 @@
 package io.fintrospect.todo
 
-import com.twitter.finagle.http.Method.{Delete, Patch, Post}
+import com.twitter.finagle.http.Method.{Delete, Get, Patch, Post}
 import com.twitter.finagle.http.Request
 import com.twitter.finagle.http.Status._
 import com.twitter.util.Await
@@ -14,9 +14,27 @@ class TodosAcceptanceTest extends FunSpec with ShouldMatchers {
 
   val svc = new TodoApp(new TodoDb("http://local/todos")).service
 
+  describe("not found conditions") {
+    it("lookup non existent todo") {
+      responseTo(Request(Get, "/todos/123"))._1 shouldBe NotFound
+    }
+
+    it("delete non existent todo") {
+      responseTo(Request(Delete, "/todos/123"))._1 shouldBe NotFound
+    }
+
+    it("update non existent todo") {
+      val request = Request(Patch, "/todos/123")
+      request.contentType = ContentTypes.APPLICATION_JSON.value
+      request.contentString = """{"title":"bob2"}"""
+
+      responseTo(request)._1 shouldBe NotFound
+    }
+  }
+
   describe("todo lifecycle") {
     it("starts with no todos") {
-      responseTo(Request("/todos")) shouldBe(Ok, JsonFormat.array())
+      responseTo(Request(Get, "/todos")) shouldBe(Ok, JsonFormat.array())
     }
 
     var newId: String = null
@@ -32,8 +50,14 @@ class TodosAcceptanceTest extends FunSpec with ShouldMatchers {
       newId = (response._2 \ "id").asInstanceOf[JString].values
     }
 
+    it("can get todo by id") {
+      val response = responseTo(Request(Get, "/todos/" + newId))
+      response._1 shouldBe Ok
+      response._2 \ "title" shouldBe JsonFormat.string("bob")
+    }
+
     it("list contains new todo") {
-      val response = responseTo(Request("/todos"))
+      val response = responseTo(Request(Get, "/todos"))
       response._1 shouldBe Ok
       response._2 \ "title" shouldBe JsonFormat.string("bob")
     }
@@ -59,7 +83,41 @@ class TodosAcceptanceTest extends FunSpec with ShouldMatchers {
     }
 
     it("list is empty again") {
-      responseTo(Request("/todos")) shouldBe(Ok, JsonFormat.array())
+      responseTo(Request(Get, "/todos")) shouldBe(Ok, JsonFormat.array())
+    }
+  }
+
+  describe("delete all") {
+    it("starts with no todos") {
+      responseTo(Request(Get, "/todos")) shouldBe(Ok, JsonFormat.array())
+    }
+
+    var newId: String = null
+
+    it("saving a todo") {
+      val request = Request(Post, "/todos")
+      request.contentType = ContentTypes.APPLICATION_JSON.value
+      request.contentString = """{"title":"bob"}"""
+
+      val response = responseTo(request)
+      response._1 shouldBe Created
+      response._2 \ "title" shouldBe JsonFormat.string("bob")
+      newId = (response._2 \ "id").asInstanceOf[JString].values
+    }
+
+    it("list contains new todo") {
+      val response = responseTo(Request(Get, "/todos"))
+      response._1 shouldBe Ok
+      response._2 \ "title" shouldBe JsonFormat.string("bob")
+    }
+
+    it("delete all") {
+      val response = responseTo(Request(Delete, "/todos"))
+      response._1 shouldBe Ok
+    }
+
+    it("list is empty again") {
+      responseTo(Request(Get, "/todos")) shouldBe(Ok, JsonFormat.array())
     }
   }
 
